2a36cdb30e807b2565a8c38686289f16051b362f
Fariz23031998 <xuramovf1888@gmail.com>
2026-01-18 00:36:24 +0500

    testing 5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,7 +33,7 @@ function AppRoutes() {
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter basename="/regos-partner-bot/admin">
+      <BrowserRouter basename="/admin">
         <AppRoutes />
       </BrowserRouter>
     </AuthProvider>
